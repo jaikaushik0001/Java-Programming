e35b3d35006ee333dfea1a6bf43d6249e9403d82
Jai Kaushik <jaisharma8009@gmail.com>
2025-12-29 23:48:02 +0530

    Factorail

---
+++ src/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factorial {
+    static void factorial(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int fact = 1;
+        for (int i=num;i>=1;i--){
+            fact*=i;
+        }
+        System.out.println("Factorial is: "+fact);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ASCII_Codes as = new ASCII_Codes();
-        as.ASCII();
+//        ASCII_Codes.ASCII();
+        Factorial.factorial();
     }
 }
