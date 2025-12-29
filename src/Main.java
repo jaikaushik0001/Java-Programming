dd66ff1726e4eb04eba8d179c97daca11ac2c95c
Jai Kaushik <jaisharma8009@gmail.com>
2025-12-29 23:58:53 +0530

    Palindrome.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
 //        ASCII_Codes.ASCII();
-        Factorial.factorial();
+//        Factorial.factorial();
+        Palindrome.palindrome();
     }
 }
---
+++ src/Palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    static void palindrome(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String s = sc.next();
+        boolean flag = true;
+        for (int i=0;i<s.length()/2;i++){
+            if (s.charAt(i) == s.charAt(s.length() - i - 1)){
+                flag = true;
+            }
+            else{
+                flag = false;
+            }
+        }
+        if (flag == true){
+            System.out.println("String is palindrome");
+        }
+        else{
+            System.out.println("String isn't a palindrome");
+        }
+    }
+}
