e84101bdd4d144aba3bdeb0d3b07a79096f45b69
Jai Kaushik <jaisharma8009@gmail.com>
2026-01-02 23:58:00 +0530

    Rearranging the Array elements to left of the array

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,6 @@ public class Main {
 //        CountVowels.countVowels();
 //        StringDivisionInEqualParts.stringDivision();
 //        DummyThread.dummyThread();
-       Rotate2DArray.Rotate2DArray();
+      ReaarangeEvenNumbersToLeft.Rearrange();
    }
 }
---
+++ src/ReaarangeEvenNumbersToLeft.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ReaarangeEvenNumbersToLeft {
+    static void Rearrange(){
+        int a[] = {1,2,3,4,5,6,7,8,9};
+        int index = 0;
+        int left = 0;
+        int right = a.length - 1;
+        while (left<=right){
+            if (a[left]%2 ==0){
+                left++;
+            }
+            else if (a[right] != 0){
+                right--;
+            }
+            else{
+                int temp = a[left];
+                a[left] = a[right];
+                a[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        System.out.println("The Rearranged Array is: "+ Arrays.toString(a));
+    }
+}
