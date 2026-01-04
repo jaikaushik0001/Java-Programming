3b6842005c2a543541010764846b3ac4549cf371
Jai Kaushik <jaisharma8009@gmail.com>
2026-01-04 23:45:35 +0530

    Reaarangement of Even numbers to the left

--- src/ReaarangeEvenNumbersToLeft.java
+++ src/ReaarangeEvenNumbersToLeft.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class ReaarangeEvenNumbersToLeft {
     static void Rearrange(){
-        int a[] = {1,2,3,4,5,6,7,8,9};
+        int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
         int left = 0;
         int right = a.length - 1;
         while (left<=right){
