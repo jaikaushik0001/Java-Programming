18406f4c2860cec268ce7834d0bfd7139a312b22
Jai Kaushik <jaisharma8009@gmail.com>
2026-01-04 23:43:24 +0530

    Kth smallest element in the array

--- src/KthLargestElement.java
+++ src/KthLargestElement.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class KthLargestElement {
     static void largest(){
         int b[] = {1,2,3,2,2,5,7,4,5,6};
-        Scanner sc = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println("Enter Value of K: ");
-        int k = sc.nextInt();
+        int k = s.nextInt();
         PriorityQueue <Integer> p = new PriorityQueue<>(Collections.reverseOrder());
         for (int i: b){
             p.add(i);
