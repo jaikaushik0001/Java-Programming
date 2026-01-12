3bb46ecfb619f7aa1625cb24bc6d8faae146ae6c
Jai Kaushik <jaisharma8009@gmail.com>
2026-01-12 23:56:31 +0530

    Changes

--- src/Sorted2DArray.java
+++ src/Sorted2DArray.java
@@ -12,7 +12,7 @@ public class Sorted2DArray {
                 {19, 18, 17}
         };
         int columnToSortBy=1;
-        System.out.println("Original trix:");
+        System.out.println("Original Matrix:");
         printMatrix(matrix);
         sortByColumn(matrix,columnToSortBy);
         System.out.println("Sorted Matrix by Column " + columnToSortBy + ":");
