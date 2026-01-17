29f1af3fbfa43d85623cfe3eaae6f3a8211e5285
Jai Kaushik <jaisharma8009@gmail.com>
2026-01-17 23:45:20 +0530

    Changes

--- src/Multithreading/SynchronizationDummy/MyThread.java
+++ src/Multithreading/SynchronizationDummy/MyThread.java
@@ -9,7 +9,7 @@ public class MyThread extends Thread{
 
     @Override
     public void run() {
-        for (int i=0;i<100;i++){
+        for (int i=0;i<1000;i++){
             counter.increment();
         }
     }
